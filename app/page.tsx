7d26aa2b97a65a574094d2251aca533582a8d6e9
kirubel12 <kirubelhabte1991@gmail.com>
2024-09-01 07:02:04 -0700

    Prisma setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import LoginButton from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  const user = db.user.count();
+  console.log(user);
   return (
     <main className="flex h-full flex-col items-center justify-center bg-[#ffbb5e]">
       <div className="space-y-6 text-center">
